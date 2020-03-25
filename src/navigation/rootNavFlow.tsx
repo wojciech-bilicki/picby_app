34ed673e0235a2a3ad82bce4d13d86193cda025e
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-03-25 17:22:33 +0100

    add loading screen

--- src/navigation/rootNavFlow.tsx
+++ src/navigation/rootNavFlow.tsx
@@ -2,8 +2,12 @@ import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 import IntroductionSceneStack from '../views/intruduction/introductionScene';
 import AuthStackNav from '../views/auth/AuthStackNav';
 import {ParentDrawer, ChildDrawer} from './MenuDrawers';
+import LoadingScreen from '../views/loading/LoadingScreen';
 
 const switchContainer = createSwitchNavigator({
+  Loading: {
+    screen: LoadingScreen,
+  },
   Intro: {
     screen: IntroductionSceneStack,
   },
---
+++ src/views/loading/LoadingScreen.tsx
@@ -0,0 +1,95 @@
+import React, {useEffect} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import PicbyLogo from '../../common/images/PICBY.svg';
+import {NavigationStackProp} from 'react-navigation-stack';
+import {getUserTokenFromAsyncStorage} from '../../easyPeasy/auth/login/utils';
+
+const {width: vw} = Dimensions.get('window');
+
+type Props = {
+  navigation: NavigationStackProp;
+};
+
+const LoadingScreen: React.FC<Props> = ({navigation}) => {
+  const navigateToOtherScreen = (screenName: string) => {
+    navigation.navigate({routeName: screenName});
+  };
+  useEffect(() => {
+    const userToken = getUserTokenFromAsyncStorage();
+    setTimeout(() => {
+      if (!userToken) {
+        navigateToOtherScreen('Login');
+      } else {
+        navigateToOtherScreen('Intro');
+      }
+    }, 300);
+  }, []);
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.headerWrapperThirdScreen}>
+          <Text style={[styles.headerText, styles.headerTextThirdScreen]}>
+            {'ŚWIAT OCZAMI DZIECKA'}
+          </Text>
+          <PicbyLogo style={[styles.logo, styles.logoThirdScreen]} />
+        </View>
+        <View style={styles.content}>
+          <ActivityIndicator size={120} color={'#3180AE'} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 0.134 * vw,
+  },
+  headerWrapperThirdScreen: {
+    flexDirection: 'column-reverse',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontFamily: 'OpenSans-Bold',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 0.056 * vw,
+    lineHeight: 0.078 * vw,
+    textAlign: 'center',
+    color: '#074782',
+    letterSpacing: 0.7,
+  },
+  headerTextThirdScreen: {
+    marginTop: 0.0262 * vw,
+  },
+  logo: {
+    minWidth: 0.65 * vw,
+    minHeight: 0.21 * vw,
+    resizeMode: 'contain',
+    marginTop: 0.0562 * vw,
+  },
+  logoThirdScreen: {
+    marginTop: 0.03 * vw,
+  },
+  text: {},
+  content: {
+    maxWidth: 0.9 * vw,
+    minHeight: 0.331 * vw,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 0.2 * vw,
+  },
+});
+
+export default LoadingScreen;
