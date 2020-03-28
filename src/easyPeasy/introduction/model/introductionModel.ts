7040370bb100ffaa4c846c95227831c2e94f433e
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-03-28 15:04:36 +0100

    intro - reset state to default on redirect

--- src/easyPeasy/introduction/model/introductionModel.ts
+++ src/easyPeasy/introduction/model/introductionModel.ts
@@ -1,13 +1,17 @@
-import {Action, action} from 'easy-peasy';
+import {Action, action, thunk, Thunk} from 'easy-peasy';
 
 export interface IntroStoreModel {
   activeScreenNumber: number;
   changeActiveScreenNumber: Action<IntroStoreModel, number>;
+  resetActiveScreenNumberToDefault: Thunk<IntroStoreModel, number>;
 }
 const IntroductionModel: IntroStoreModel = {
   activeScreenNumber: 1,
   changeActiveScreenNumber: action((state, payload) => {
     state.activeScreenNumber = payload;
+  }),
+  resetActiveScreenNumberToDefault: thunk(actions => {
+    actions.changeActiveScreenNumber(1);
   }),
 };
 
